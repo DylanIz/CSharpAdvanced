93cdea3f9bf7fdcb599330c9f661ce607465bb2a
Dylan <dylan.drumgoole@fenergo.com>
2024-03-04 15:59:29 +0000

    Setup

---
+++ HRAdinAPI/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace HRAdinAPI;
+
+interface IEmployee
+{
+    int Id { get; set; }
+
+    string FirstName { get; set; }
+
+    string LastName { get; set; }
+
+    decimal Salary { get; set; }
+
+}
---
+++ HRAdministration/Program.cs
@@ -0,0 +1,21 @@
+﻿using HRAdinAPI;
+
+public class Teacher : EmployeeBase
+{
+
+}
+
+public class HeadOfDepartment : EmployeeBase
+{
+
+}
+
+public class VicePrincipal : EmployeeBase
+{
+
+}
+
+public class Principal : EmployeeBase
+{
+
+}
